ab243a4f7080fd9c809c37c3f419310274d2b8ea
ronyahmmod <rony.jib@gmail.com>
2020-09-06 02:11:30 +0600

    Updates

---
+++ precache-manifest.539a5c32bd4886327c5b9320626f67ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b1924a6a47c37ce7be5e14770441ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68697c74a2a6c408b320",
+    "url": "/static/css/main.913c07f1.chunk.css"
+  },
+  {
+    "revision": "db93800fa09720f85867",
+    "url": "/static/js/2.42f37014.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42f37014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68697c74a2a6c408b320",
+    "url": "/static/js/main.388fad0e.chunk.js"
+  },
+  {
+    "revision": "2036126f19effa954b3c",
+    "url": "/static/js/runtime-main.719a4e28.js"
+  }
+]);
